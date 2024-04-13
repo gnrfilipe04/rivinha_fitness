434f2b99b3bfe5a6ed6f6d33b056f0cd0c051c0b
gnrfilipe04 <gnr_filipe@hotmail.com>
2024-04-13 15:20:00 -0300

    :sparkles: add firebase

--- lib/app/config/app_provider.dart
+++ lib/app/config/app_provider.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
 import 'package:rivinha_fitness/app/customers/customers_store.dart';
 import 'package:rivinha_fitness/app/customers/workout/workout_store.dart';
 import 'package:rivinha_fitness/app/home/home_store.dart';
+import 'package:rivinha_fitness/firebase_options.dart';
 
 GetIt provider = GetIt.instance;
 
@@ -15,6 +17,10 @@ Future<void> setupProvider() async {
 
 Future<void> setupApp() async {
   await setupProvider();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
